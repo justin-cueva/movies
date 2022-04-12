e22bbf8eccfb3cf86ef8efa52f2a71ad70f1fac5
justincueva02 <justincueva02@gmail.com>
2022-04-12 15:32:39 -0700

    filtering our movie objects

--- src/reducers/moviesReducer.ts
+++ src/reducers/moviesReducer.ts
@@ -3,7 +3,11 @@ type Actions = { type: "GOT_MOVIES"; payload: any };
 export default (state = {}, action: Actions) => {
   switch (action.type) {
     case "GOT_MOVIES":
-      return { ...state, [action.payload[0]]: action.payload[1] };
+      const newArrayOfMovies = action.payload[1].map((movie: any) => {
+        return { id: movie.id, image: movie.image, title: movie.title };
+      });
+
+      return { ...state, [action.payload[0]]: newArrayOfMovies };
     default:
       return state;
   }
