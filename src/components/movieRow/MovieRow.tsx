6700c234cd0b9dddba11fb35c4b3959c301ebd7b
justincueva02 <justincueva02@gmail.com>
2022-04-12 15:57:29 -0700

    displaying our first row of images

--- src/components/movieRow/MovieRow.tsx
+++ src/components/movieRow/MovieRow.tsx
@@ -6,14 +6,19 @@ import fetchMovies from "../../actions/fetchMovies";
 
 const MovieRow = ({ fetchMovies, movies }: PropsFromRedux) => {
   useEffect(() => {
-    fetchMovies(5, "disney");
+    fetchMovies(8, "disney");
     console.log("fetching movies");
   }, []);
 
   return (
     <div className="movie-row">
-      <h2>MovieRow</h2>
-      <div className="conatiner--movies"></div>
+      <h2>Disney</h2>
+      <div className="conatiner--movies">
+        {movies.disney &&
+          movies.disney.map((movie: any) => {
+            return <img key={movie.id} className="movie" src={movie.image} />;
+          })}
+      </div>
     </div>
   );
 };
