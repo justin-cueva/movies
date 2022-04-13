f8730f319fca9e17013f66604fa837b9d71b7e54
justincueva02 <justincueva02@gmail.com>
2022-04-13 11:25:07 -0700

    made cool buttons that are animated

--- src/components/movieRow/MovieRow.js
+++ src/components/movieRow/MovieRow.js
@@ -3,7 +3,7 @@ import { connect, ConnectedProps } from "react-redux";
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 import "../../MovieRow.css";
 import fetchMovies from "../../actions/fetchMovies";
@@ -21,25 +21,32 @@ const MovieRow = ({ fetchMovies, movies, company }) => {
       {movies[company] && (
         <div className="buttons">
           <h2> {company}</h2>
-          {position === 0 ? null : (
-            <button onClick={() => setPosition((prev) => prev - 1)}>
-              left
-            </button>
-          )}
-          {position === 2 ? null : (
-            <button onClick={() => setPosition((prev) => prev + 1)}>
-              right
-            </button>
-          )}
         </div>
       )}
       {movies[company] && (
         <div className="container--multi-row">
+          {position === 0 ? null : (
+            <button
+              className="btn--left"
+              onClick={() => setPosition((prev) => prev - 1)}
+            >
+              <BsChevronCompactLeft />
+            </button>
+          )}
+          {position === 2 ? null : (
+            <button
+              className="btn--right"
+              onClick={() => setPosition((prev) => prev + 1)}
+            >
+              <BsChevronCompactRight />
+            </button>
+          )}
           {[0, 1, 2].map((index) => {
             const start = index * 9;
             const end = (index + 1) * 9;
             return (
               <div
+                key={index}
                 className={`conatiner--movies row-absolute-right--${index} transformed--${position}`}
               >
                 {movies[company].slice(start, end).map((movie) => {
