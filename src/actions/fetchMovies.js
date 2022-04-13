d1255af0ca9931fcdff2ba53907dad92e964d785
justincueva02 <justincueva02@gmail.com>
2022-04-12 21:01:40 -0700

    viewing more movies in a row

--- src/actions/fetchMovies.js
+++ src/actions/fetchMovies.js
@@ -1,6 +1,10 @@
+// keys
+// k_yg2kd715
+// k_2xj015ok
+
 export default (amount, company) => async (dispatch) => {
   const response = await fetch(
-    `https://imdb-api.com/API/AdvancedSearch/k_2xj015ok?companies=${company}&count=${amount}`
+    `https://imdb-api.com/API/AdvancedSearch/k_yg2kd715?companies=${company}&count=${amount}`
   );
 
   const { results } = await response.json();
--- src/components/movieRow/MovieRow.js
+++ src/components/movieRow/MovieRow.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
@@ -8,27 +8,38 @@ import "../../MovieRow.css";
 import fetchMovies from "../../actions/fetchMovies";
 
 const MovieRow = ({ fetchMovies, movies, company }) => {
+  const [transformed, setTransformed] = useState(false);
+  const [position, setPosition] = useState(0);
+
   useEffect(() => {
-    fetchMovies(8, company);
+    fetchMovies(21, company);
     console.log("fetching movies");
   }, []);
 
   return (
     <div className="movie-row">
-      <h2>{company}</h2>
-      <div className="conatiner--movies">
-        {movies[company] &&
-          movies[company].map((movie) => {
+      {movies[company] && (
+        <div className="buttons">
+          <h2> {company}</h2>
+          <button onClick={() => setPosition((prev) => prev - 1)}>left</button>
+          <button onClick={() => setPosition((prev) => prev + 1)}>right</button>
+        </div>
+      )}
+      {movies[company] && (
+        <div className={`conatiner--movies transformed--${position}`}>
+          {movies[company].map((movie) => {
             return (
               <LazyLoadImage
-                effect="blur"
                 key={movie.id}
                 className="movie"
+                effect="blur"
                 src={movie.image}
+                placeholder={<div className="movie-placeholder" />}
               />
             );
           })}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
